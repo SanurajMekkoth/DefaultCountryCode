7ad6a1958abc3782dc5a0eee6091600ed0b324ae
Siddhant-Speridian <84002443+Siddhant-Speridian@users.noreply.github.com>
2022-01-19 22:22:03 +0530

    iOS method channel implementation

--- ios/Classes/SwiftDefaultCountryCodePlugin.swift
+++ ios/Classes/SwiftDefaultCountryCodePlugin.swift
@@ -1,11 +1,49 @@
 import Flutter
 import UIKit
+import CoreTelephony
+
 
 public class SwiftDefaultCountryCodePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "default_country_code", binaryMessenger: registrar.messenger())
     let instance = SwiftDefaultCountryCodePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+      
+      
+      channel.setMethodCallHandler { (methodCall, result) in
+          if methodCall.method == "detectSIMCountry" {
+              
+              let networkInfo = CTTelephonyNetworkInfo()
+              if #available(iOS 12.0, *) {
+                  let carrier = networkInfo.serviceSubscriberCellularProviders?.map({ $0.1 }).first(where: { $0.isoCountryCode != nil })
+                  let isoCode = carrier?.isoCountryCode?.uppercased()
+                  result(isoCode)
+              } else {
+                  // Fallback on earlier versions
+                  let carrier = networkInfo.subscriberCellularProvider
+                  let isoCode = carrier?.isoCountryCode?.uppercased()
+                  result(isoCode)
+                  
+              }
+          } else if methodCall.method == "detectNetworkCountry" {
+              
+              let networkInfo = CTTelephonyNetworkInfo()
+              if #available(iOS 12.0, *) {
+                  let carrier = networkInfo.serviceSubscriberCellularProviders?.map({ $0.1 }).first(where: { $0.isoCountryCode != nil })
+                  let isoCode = carrier?.isoCountryCode?.uppercased()
+                  result(isoCode)
+              } else {
+                  // Fallback on earlier versions
+                  let carrier = networkInfo.subscriberCellularProvider
+                  let isoCode = carrier?.isoCountryCode?.uppercased()
+                  result(isoCode)
+                  
+              }
+          } else if methodCall.method == "detectLocaleCountry" {
+              let locale = Locale.current
+              result(locale.regionCode)
+          }
+      }
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
